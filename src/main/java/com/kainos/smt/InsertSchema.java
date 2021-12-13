d6d5f529437645cf5b10932bc1b9a3640b8567d9
manquak <manquak@gmail.com>
2021-12-13 16:38:48 +0100

    schema from avro file

--- src/main/java/com/kainos/smt/InsertSchema.java
+++ src/main/java/com/kainos/smt/InsertSchema.java
@@ -1,24 +1,40 @@
 package com.kainos.smt;
 
+import io.confluent.connect.avro.AvroData;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.transforms.Transformation;
+import org.apache.kafka.connect.transforms.util.SimpleConfig;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
+import static io.confluent.connect.avro.AvroDataConfig.SCHEMAS_CACHE_SIZE_DEFAULT;
 import static org.apache.kafka.connect.transforms.util.Requirements.requireMap;
 
 public class InsertSchema<R extends ConnectRecord<R>> implements Transformation<R> {
-
     public static final String OVERVIEW_DOC = "Insert schema to the payload";
     private static final String PURPOSE = "adding schema to record";
-    public static final ConfigDef CONFIG_DEF = new ConfigDef();
+
+    private final AvroData avroData = new AvroData(SCHEMAS_CACHE_SIZE_DEFAULT);
+
+    private interface ConfigName {
+        String AVRO_SCHEMA_PATH = "avro.schema.path";
+    }
+
+    public static final ConfigDef CONFIG_DEF = new ConfigDef()
+            .define(ConfigName.AVRO_SCHEMA_PATH, ConfigDef.Type.STRING, "", ConfigDef.Importance.HIGH,
+                    "path to avsc file holding schema information about the records");
+    private String avroSchemaPath;
+
     @Override
     public void configure(Map<String, ?> props) {
+        final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
+        avroSchemaPath = config.getString(ConfigName.AVRO_SCHEMA_PATH);
     }
 
     @Override
@@ -32,8 +48,8 @@ public class InsertSchema<R extends ConnectRecord<R>> implements Transformation<
 
     @Override
     public R apply(R record) {
-        final Map<String,?> value = requireMap(record.value(),PURPOSE);
-        Schema updatedSchema = makeUpdatedSchema();
+        final Map<String, ?> value = requireMap(record.value(), PURPOSE);
+        Schema updatedSchema = convertAvroSchemaToConnectSchema(avroSchemaPath);
 
         final Struct updatedValue = new Struct(updatedSchema);
 
@@ -47,13 +63,22 @@ public class InsertSchema<R extends ConnectRecord<R>> implements Transformation<
         return newRecord(record, updatedSchema, updatedValue);
     }
 
-    private Schema makeUpdatedSchema() {
-        final SchemaBuilder builder = SchemaBuilder.struct()
-                .name("json_schema")
-                .field("first",Schema.INT64_SCHEMA)
-                .field("second",Schema.STRING_SCHEMA);
+    private Schema convertAvroSchemaToConnectSchema(String pathToAvscFile) {
+        Schema schema = null;
+        try {
+            schema = tryToConvertAvroSchemaToConnectSchema(pathToAvscFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        return builder.build();
+        return schema;
+    }
+
+    private Schema tryToConvertAvroSchemaToConnectSchema(String pathToAvscFile) throws IOException {
+        org.apache.avro.Schema schema = new org.apache.avro.Schema.Parser().parse(
+                new File(pathToAvscFile)
+        );
+        return avroData.toConnectSchema(schema);
     }
 
     protected R newRecord(R record, Schema updatedSchema, Struct updatedValue) {
